58d5ae92b28ea11f2cd02c237e69e82c491dedde
Thiago Marcos P. Santos <thiago.santos@intel.com>
2013-11-28 17:03:48 +0200

    [SysApps] Add a stub for the Device Capabilities API
    This patch adds the basic bits for starting the development of the
    Device Capabilties API:
    
    - The IDL files to generate the data serializers.
    - The JavaScript shim file, initializing the internal extension system
    - An Extension class and an Instance class.
    - Register the extension on the XWalkBrowserMainParts (only desktop ATM).

---
+++ sysapps/device_capabilities_new/device_capabilities_extension.cc
@@ -0,0 +1,33 @@
+// Copyright (c) 2013 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "xwalk/sysapps/device_capabilities_new/device_capabilities_extension.h"
+
+#include "grit/xwalk_sysapps_resources.h"
+#include "ui/base/resource/resource_bundle.h"
+
+namespace xwalk {
+namespace sysapps {
+
+DeviceCapabilitiesExtension::DeviceCapabilitiesExtension() {
+  set_name("xwalk.experimental.system");
+  set_javascript_api(ResourceBundle::GetSharedInstance().GetRawDataResource(
+      IDR_XWALK_SYSAPPS_DEVICE_CAPABILITIES_NEW_API).as_string());
+}
+
+DeviceCapabilitiesExtension::~DeviceCapabilitiesExtension() {}
+
+XWalkExtensionInstance* DeviceCapabilitiesExtension::CreateInstance() {
+  return new DeviceCapabilitiesInstance();
+}
+
+DeviceCapabilitiesInstance::DeviceCapabilitiesInstance()
+  : handler_(this) {}
+
+void DeviceCapabilitiesInstance::HandleMessage(scoped_ptr<base::Value> msg) {
+  handler_.HandleMessage(msg.Pass());
+}
+
+}  // namespace sysapps
+}  // namespace xwalk
