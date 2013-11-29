cd2d124aaab433a27a0a69c406f86be2c200c0c6
Thiago Marcos P. Santos <thiago.santos@intel.com>
2013-11-29 15:22:22 +0200

    [SysApps] Add an data provider abstraction for the CPU information
    The data provider will access the underlying platform and fill a SystemCPU
    struct. We are using Chromium classes to retrieve this information, which is
    platform independent. In the following patch we will be adding a platform
    specialization for querying the CPU load.
    
    Note that the SystemCPU struct is not defined anywhere. By adding SystemCPU dictionary
    interface to the IDL file, we instruct the generators to generate the header and
    code for this struct, also a function to serialize the struct as a message we can send to
    the JavaScript shim.

---
+++ sysapps/device_capabilities_new/cpu_info_provider.h
@@ -0,0 +1,41 @@
+// Copyright (c) 2013 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef XWALK_SYSAPPS_DEVICE_CAPABILITIES_NEW_CPU_INFO_PROVIDER_H_
+#define XWALK_SYSAPPS_DEVICE_CAPABILITIES_NEW_CPU_INFO_PROVIDER_H_
+
+#include <string>
+
+#include "base/memory/scoped_ptr.h"
+#include "base/memory/singleton.h"
+#include "xwalk/sysapps/device_capabilities_new/device_capabilities.h"
+
+namespace xwalk {
+namespace sysapps {
+
+using jsapi::device_capabilities::SystemCPU;
+
+class CPUInfoProvider {
+ public:
+  static CPUInfoProvider* GetInstance();
+
+  ~CPUInfoProvider();
+
+  scoped_ptr<SystemCPU> cpu_info() const;
+
+ private:
+  CPUInfoProvider();
+
+  int number_of_processors_;
+  std::string processor_architecture_;
+
+  friend struct DefaultSingletonTraits<CPUInfoProvider>;
+
+  DISALLOW_COPY_AND_ASSIGN(CPUInfoProvider);
+};
+
+}  // namespace sysapps
+}  // namespace xwalk
+
+#endif  // XWALK_SYSAPPS_DEVICE_CAPABILITIES_NEW_CPU_INFO_PROVIDER_H_
