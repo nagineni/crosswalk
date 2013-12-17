c1c50aff005d5b24833a494ad1e5acbe09760758
Sudarsana Nagineni <sudarsana.nagineni@intel.com>
2013-12-17 21:51:59 +0200

    [SysApps] Implement the Device Capabilities getAVCodecs() for Android
    The data provider will get the list of supported codecs on android from
    the MediaCodecBridge.

--- sysapps/device_capabilities_new/av_codecs_provider_android.cc
+++ sysapps/device_capabilities_new/av_codecs_provider_android.cc
@@ -4,8 +4,17 @@
 
 #include "xwalk/sysapps/device_capabilities_new/av_codecs_provider_android.h"
 
+#include "base/strings/string_util.h"
+#include "media/base/android/media_codec_bridge.h"
+
+#include <set>
+#include <string>
+
 namespace xwalk {
 namespace sysapps {
+
+const char* const kAudioCodecs[] = {"ALAC", "MP3", "AMRNB", "AMRWB", "AAC", "G711", "VORBIS", "WMA", "FLAC", "OPUS"};
+const char* const kVideoCodecs[] = {"H263", "H264", "MPEG4", "AVC", "WMV", "VP8", "Theora"};
 
 static base::LazyInstance<AVCodecsProviderAndroid>::Leaky
     g_storage_info_provider = LAZY_INSTANCE_INITIALIZER;
@@ -15,8 +24,41 @@ AVCodecsProviderAndroid::AVCodecsProviderAndroid() {}
 AVCodecsProviderAndroid::~AVCodecsProviderAndroid() {}
 
 scoped_ptr<SystemAVCodecs> AVCodecsProviderAndroid::av_codecs() const {
-  NOTIMPLEMENTED();
-  return make_scoped_ptr(new SystemAVCodecs);
+  scoped_ptr<SystemAVCodecs> av_codecs(new SystemAVCodecs);
+  std::vector<media::MediaCodecBridge::CodecsInfo> codecs_info;
+  media::MediaCodecBridge::GetCodecsInfo(&codecs_info, true);
+
+  for (size_t i = 0; i < codecs_info.size(); ++i) {
+    bool handled = false;
+    std::string codec_name = StringToUpperASCII(codecs_info[i].name);
+    for (size_t i = 0; i < arraysize(kAudioCodecs); ++i) {
+      if (string16::npos != codec_name.find(kAudioCodecs[i])) {
+        linked_ptr<AudioCodec> audio_codec(new AudioCodec);
+        audio_codec->format = std::string(kAudioCodecs[i]);
+        av_codecs->audio_codecs.push_back(audio_codec);
+        handled = true;
+        break;
+      }
+    }
+
+    if (handled)
+      continue;
+
+    for (size_t i = 0; i < arraysize(kVideoCodecs); ++i) {
+      if (string16::npos != codec_name.find(kVideoCodecs[i])) {
+        linked_ptr<VideoCodec> video_codec(new VideoCodec);
+        video_codec->format = std::string(kVideoCodecs[i]);
+        // FIXME(qjia7): find how to get hwAccel value
+        video_codec->hw_accel = false;
+        video_codec->encode = codecs_info[i].isEncoder;
+        av_codecs->video_codecs.push_back(video_codec);
+        handled = true;
+        break;
+      }
+    }
+  }
+
+  return av_codecs.Pass();
 }
 
 // static
