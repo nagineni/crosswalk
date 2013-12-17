4d175f5ba35541ca4b2f0da6f1d00bfcaaa0b81e
Thiago Marcos P. Santos <thiago.santos@intel.com>
2013-12-17 18:00:33 +0200

    [SysApps] Introduce the SysAppsManager
    This class will be the single entry point for registering all
    the SysApps extensions. It should be the only class of the "sysapps"
    namespace exposed to other subsystems.
    
    In the future it will also manage the lifecycle of data providers (platform
    abstractions for extensions like DeviceCapabilities).

---
+++ sysapps/common/sysapps_manager.h
@@ -0,0 +1,27 @@
+// Copyright (c) 2013 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef XWALK_SYSAPPS_COMMON_SYSAPPS_MANAGER_H_
+#define XWALK_SYSAPPS_COMMON_SYSAPPS_MANAGER_H_
+
+#include "xwalk/extensions/common/xwalk_extension_vector.h"
+
+namespace xwalk {
+namespace sysapps {
+
+using extensions::XWalkExtensionVector;
+
+class SysAppsManager {
+ public:
+  SysAppsManager();
+  ~SysAppsManager();
+
+  void CreateExtensionsForUIThread(XWalkExtensionVector* extensions);
+  void CreateExtensionsForExtensionThread(XWalkExtensionVector* extensions);
+};
+
+}  // namespace sysapps
+}  // namespace xwalk
+
+#endif  // XWALK_SYSAPPS_COMMON_SYSAPPS_MANAGER_H_
